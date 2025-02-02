960edb734be357743f674f1976d5c3ace777f814
giginet <giginet.net@gmail.com>
2025-02-02 16:54:36 +0900

    Use node-archiver to build directory

--- src/archiver.ts
+++ src/archiver.ts
@@ -1,55 +1,22 @@
 import * as fs from 'fs'
-import * as path from 'path'
-import * as zlib from 'zlib'
-import { Readable } from 'stream'
+import archiver from 'archiver'
 
 class ZipArchiver {
   async archive(directory: string, outputPath: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
       const output = fs.createWriteStream(outputPath)
-      const gzip = zlib.createGzip()
+      const archive = archiver('zip', {
+        zlib: { level: 9 }
+      })
 
       output.on('close', () => resolve(outputPath))
-      gzip.on('error', (err) => reject(err))
+      archive.on('error', (err: Error) => reject(err))
 
-      gzip.pipe(output)
+      archive.pipe(output)
 
-      // Function to recursively read files
-      const readFiles = (dir: string, fileList: string[] = []): string[] => {
-        const files = fs.readdirSync(dir)
-        files.forEach((file) => {
-          const filePath = path.join(dir, file)
-          const stat = fs.statSync(filePath)
-          if (stat.isDirectory()) {
-            readFiles(filePath, fileList)
-          } else {
-            fileList.push(filePath)
-          }
-        })
-        return fileList
-      }
+      archive.directory(directory, false)
 
-      const files = readFiles(directory)
-
-      // Create a readable stream from the list of files
-      const fileStream = Readable.from(files)
-
-      fileStream.on('data', (filePath: string) => {
-        const relativePath = path.relative(directory, filePath)
-        const fileContent = fs.readFileSync(filePath)
-        const header = Buffer.from(`File: ${relativePath}\n`)
-        gzip.write(header)
-        gzip.write(fileContent)
-        gzip.write(Buffer.from('\n'))
-      })
-
-      fileStream.on('end', () => {
-        gzip.end()
-      })
-
-      fileStream.on('error', (err) => {
-        reject(err)
-      })
+      archive.finalize()
     })
   }
 }
